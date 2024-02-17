13c81e0824813069c6904d40df59f0282db51649
Bharat Deleep Nair <bharatdeleepnair@Bharats-MacBook-Air.local>
2024-02-17 22:20:27 +0530

    first commit

--- src/Card.js
+++ src/Card.js
@@ -1,63 +1,67 @@
 import React from 'react';
 
 const styles = {
-    Card: {
-      position: 'relative',
-      top: '0px',
-      left: '0px',
-      right: '0px',
-      height: '550px', 
-      backgroundColor: '#1d2732',
-      borderRadius: '78px',
-      boxSizing: 'border-box',
-      boxShadow: '0px 0px 30px black',
-      opacity: 1,
-      zIndex: 1,
-
-    },
+  Card: {
+    position: 'relative',
+    height: '550px', 
+    backgroundColor: '#1d2732',
+    borderRadius: '78px',
+    boxShadow: '0px 0px 30px black',
+    opacity: 1,
+  },
   
-    UCard: {
-      position: 'absolute',
-      top: '0px',
-      height: '10%',
-      backgroundColor: '#1d2732',
-      opacity: 1,
-      zIndex: 2, 
-      width: '100%'
-    },
+  UCard: {
+    position: 'absolute',
+    top: '0px',
+    height: '15%',
+    backgroundColor: '#1d2732',
+    opacity: 1,
+    width: '100%'
+  },
 
-    Movie_Poster_Details: {
-        zIndex: 3
-    }
-  };
+  MoviePoster: {
+    position: 'relative',
+    display: 'inline-block',
+    width: '40vw'
+  },
+
+  MoviePosterDetails: {
+    position: 'relative',
+    display: 'inline-block',
+    width: '50vw',
+    verticalAlign: 'center',
+    padding: '20px',
+    color: '#ffffff',
+  }
+};
   
 
 const Card = ({ Movie, children }) => {
   return (
-    <div className='Card'>
-    <div >
+   
+      
+      <div style={styles.Card}>
+       <div className='Card'>
       <div style={styles.UCard}>
         <h2>CineMax</h2>
       </div>
-      
-      <div style={styles.Card}>
         {children}
         {Movie && (
           <div className='random-movie'>
+            <div style={styles.MoviePoster}>
             <div className='movie-poster'>
               <img src={`https://image.tmdb.org/t/p/w500${Movie.poster_path}`} alt={Movie.title} />
-              </div>
-              <div className='movie-poster-details'>
-              <div style={styles.Movie_Poster_Details}>
-                <h3>{Movie.title}</h3>
-                <p>{Movie.release_date}</p>
-                </div>
-                <p className="description">{Movie.overview}</p>
-                </div> 
+            </div>
+            </div>
+            <div style={styles.MoviePosterDetails}>
+            <div className='movie-poster-details'>
+              <h3>{Movie.title}</h3>
+              <p>{Movie.release_date}</p>
+            </div>
+              <p>{Movie.overview}</p>
+            </div>
           </div>
         )}
-        
-        </div>
       </div>
     </div>
   );
